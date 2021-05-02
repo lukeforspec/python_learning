60f23d3a05e647de6ad7ef7edc48a3f5b0e91d1f
lukeatspecs <lukeatspecs@gmail.com>
2021-05-02 00:29:23 -0600

    05/02 update

--- fastest_way_to_loop.py
+++ fastest_way_to_loop.py
@@ -6,40 +6,49 @@ import numpy
 Sum the numbers from 0 to n-1 in different ways.
 """
 
+# 13.05s
+
 
 def while_loop(n=100_000_000):
     i = 0
     s = 0
-    while i < n:
+    while i < n:  # done in python
         s += i
         i += 1
     return s
+
+# 6.75s
+# python pass interation in C, which is why for is faster
 
 
 def for_loop(n=100_000_000):
     s = 0
     for i in range(n):
-        s += i
+        s += i  # done in python
     return s
 
 
+# 13.50
 def for_loop_with_increment(n=100_000_000):
     s = 0
     for i in range(n):
         s += i
-        i += 1
+        i += 1  # does not do anything but will increase the time
     return s
+
+# 12.19s
 
 
 def for_loop_with_test(n=100_000_000):
     s = 0
     for i in range(n):
         if i < n:
-            pass
+            pass  # useless check add time
         s += i
     return s
 
 
+# 18.04s
 def for_loop_with_increment_and_test(n=100_000_000):
     s = 0
     for i in range(n):
@@ -49,17 +58,24 @@ def for_loop_with_increment_and_test(n=100_000_000):
         s += i
     return s
 
+# 3.85s, use buildin instead of loops will speed up, not memory constraint.
+
 
 def sum_range(n=100_000_000):
     return sum(range(n))
 
 
+# 9.80s, not as fast as loops
 def sum_generator(n=100_000_000):
     return sum(i for i in range(n))
+
+# 12.72s
 
 
 def sum_list_comp(n=100_000_000):
     return sum([i for i in range(n)])
+
+# 0.50s, numpy is fast! This is one C call. Might not have n memory.  memory intensive.
 
 
 def sum_numpy(n=100_000_000):
@@ -69,17 +85,21 @@ def sum_numpy(n=100_000_000):
 def sum_numpy_python_range(n=100_000_000):
     return numpy.sum(range(n))
 
+# pure math is fastest.
+
 
 def sum_math(n=100_000_000):
     return (n * (n - 1)) // 2
 
 
+# fastest way to loop in Python is not to loop, but compute ahead.
+# if have to use, use numpy and then build ins.
 def main():
     print('while loop\t\t', timeit.timeit(while_loop, number=1))
     print('for pure\t\t', timeit.timeit(for_loop, number=1))
-    # print('for inc\t\t\t', timeit.timeit(for_loop_with_increment, number=1))
-    # print('for test\t\t', timeit.timeit(for_loop_with_test, number=1))
-    # print('for inc+test\t', timeit.timeit(for_loop_with_increment_and_test, number=1))
+    print('for inc\t\t\t', timeit.timeit(for_loop_with_increment, number=1))
+    print('for test\t\t', timeit.timeit(for_loop_with_test, number=1))
+    print('for inc+test\t', timeit.timeit(for_loop_with_increment_and_test, number=1))
     print('sum range\t\t', timeit.timeit(sum_range, number=1))
     print('sum generator\t\t', timeit.timeit(sum_generator, number=1))
     print('sum list comp\t\t', timeit.timeit(sum_list_comp, number=1))
